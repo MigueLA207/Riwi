23a241a5d78fe3e2caa4fa7d4e838a1db837c901
MigueLA207 <mariasmarin981@gmail.com>
2025-05-06 18:45:33 -0500

    Add files via upload

---
+++ Riwi/ejercicios_clase/RepasoTL/funciones.py
@@ -0,0 +1,70 @@
+
+# def ejercicio1(nombre):
+#     print(f"Hola {nombre} espero que tengas un lindo dia")
+
+# def ejercicio2(num1,num2):
+#     print(f"La suma de los dos numeros es {num1 + num2}") 
+
+# def ejercicio3(num):
+#     if num % 2 == 0:
+#         print("El numero es par")
+#     else:
+#         print("Numero impar")
+
+# def ejercicio4(edad):
+#     if edad < 18:
+#         print("Menor de edad")
+#     else:
+#         print("Mayor de edad")
+
+# def ejercicio5(temperatura):
+#     print(f"La convercion de grados {temperatura} Celsius a Fahrenheit es : {(temperatura * 9/5) + 32}")
+
+# def ejercicio6(base,altura):
+#     print(f"El area del triangulo con base {base} y altura {altura} es: {(base*altura/2)} ")
+
+
+
+# def ejercicio7(numeros):
+#     mayor = 0
+#     for i in numeros:
+#         if mayor > i:
+#             mayor = mayor
+#         else:
+#             mayor=i
+#     print(f"El numero mayor es: {mayor}")
+
+
+# def ejercicio8(letra):
+#     palabra = "otorrinolaringologo"
+#     repetido = (palabra.count(letra))
+#     print(f"La letra {letra} se repite {repetido} veces")
+
+# def ejercicio9(lista):
+#     pares = [numero for numero in lista if numero % 2 == 0]
+#     print(f"lista con todo: {lista}")
+#     print(f"los pares de la lista que ingresaste son: {pares}")
+
+# def ejercicio10(palindromo):
+#     palabralista = list(palindromo)
+#     inverso = palabralista.copy()
+#     inverso.reverse()
+#     if palabralista == inverso:
+#         print("Palindromo")
+#     else:
+#         print("no palindromo")
+
+# def ejercicio11(numero):
+#     factorial = 1
+#     repite = numero
+#     for i in range (repite):
+#         factorial *= numero
+#         numero -= 1
+#     print(factorial)
+
+def ejercicio12():
+    
+
+
+
+
---
+++ Riwi/ejercicios_clase/RepasoTL/validacion_de_productos.py
@@ -0,0 +1,72 @@
+'''def suma(numero1,numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+numero1 = int(input("Digite numero 1: "))
+numero2 = int(input("Digite numero 2:  "))            
+
+print(suma(numero1,numero2))'''
+
+from funciones import *
+
+
+# #Ejercicio 1
+# nombre = input("Ingrese su nombre por favor: ")
+# ejercicio1(nombre)
+
+# #Ejercicio 2
+# num1 = int(input("Digite el numero 1: "))
+# num2 = int(input("Digite el numero 2: "))
+# ejercicio2(num1,num2)
+
+# #Ejercicio 3
+# num = int(input("Digite por favor un numero: "))
+# ejercicio3(num)
+
+# #Ejercicio 4
+
+# edad = int(input("Digite su edad por favor: "))
+# ejercicio4(edad)
+# #Ejercicio 5
+
+# temperatura = int(input("Digite temperatura en c por favor: "))
+# ejercicio5(temperatura)
+# #Ejercicio 6
+
+# base = int(input("Digite la base del triangulo: "))
+# altura = int(input("Digite la altura dl triangulo: "))
+# ejercicio6(base,altura)
+# #Ejercicio 7
+
+# numeros = []
+
+# for i in range(5):
+#     i = int(input("Digita un numero por favor para ingresarlo a la lista: "))
+#     numeros.append(i)
+
+# ejercicio7(numeros)
+
+# #Ejercicio 8
+# letra = input("Digite una letra para buscar cuantas veces se repite en la palabra (otorrinolaringologo): ")
+# ejercicio8(letra)
+
+# #Ejercicio 9
+# lista = [1,2,3,4,5]
+# ejercicio9(lista)
+
+# #Ejercicio 10
+# palindromo = input("Digite una palabra y te dire si es palindromo: ")
+# ejercicio10(palindromo)
+
+# #Ejercicio 11
+# numero = int(input("Ingrese un numero para sacarle el factorial: "))
+# ejercicio11(numero)
+
+#Ejercicio 12
+eliminar_repetidos = []
+
+for i in range(5):
+    i = int(input("Digita un numero por favor para ingresarlo a la lista: "))
+    eliminar_repetidos.append(i)
+
+ejercicio7(numeros)
